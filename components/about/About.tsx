6caf6cd87654da5419f7d3c25ae8b2a5ad1d8b58
mt_tarik <skorohodit12@gmail.com>
2023-07-07 15:43:24 +0300

    Slider

---
+++ components/about/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { Data } from "@/pages";
+
+
+export function About() {
+    return (
+       <div>
+           tdgasghdjhsajkdjasmd
+       </div>
+    );
+}
+
