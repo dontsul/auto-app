fe112daf2725b683d478759e4b07dbe369f981cd
mt_tarik <skorohodit12@gmail.com>
2023-07-08 14:55:18 +0300

    newComponent

---
+++ components/aboutSection/AboutBg.tsx
@@ -0,0 +1,13 @@
+export function AboutBg () {
+    return (
+        <div className="absolute inset-0 flex items-center justify-center">
+            <div className="h-screen">
+                <h1 className="absolute z-10 flex text-9xl text-center">
+                    <span className="bg-gradient-to-r from-red-500 to-blue-500 text-transparent bg-clip-text">
+                        A B O U T
+                    </span>
+                </h1>
+            </div>
+        </div>
+    );
+}
--- components/aboutSection/AboutSection.tsx
+++ components/aboutSection/AboutSection.tsx
@@ -1,39 +1,41 @@
 import React from "react";
-import {motion} from "framer-motion";
+import {AboutBg} from "@/components/aboutSection/AboutBg";
 
 export function AboutSection (){
 
     return(
-        <div className="grid grid-cols-12 md:col-span-3 gap-3">
-            <div className="col-span-12 flex md:col-span-6 flex-col items-center justify-center px-5 md:px-10">
-                <h3 className="text-center">
-                    <span className=" text-1xl font-semibold md:my-3 md:text-4xl md:leading-[100px]" style={{ color: 'black' }}>Elevating Vehicles For </span>
-                    <span className=" text-1xl font-semibold md:my-3 md:text-4xl md:leading-[100px]" style={{ color: '#ac9b79' }}>Max Performance</span>
-                    <span className="text-center mb-2 h-1 w-5 rounded-full bg-white" style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}>
-        At Elevated Motoring,
-        we are dedicated to providing
-        high-quality service for your
-        vehicle needs. With 35+ years
-        of combined experience in the
-        automotive industry, our knowledgeable
-        staff can assist you with OEM and aftermarket
-        solutions tailored to your vehicle’s requirements.
-        Trust us to deliver exceptional results and elevate
-        your driving experience.
-      </span>
-                </h3>
-                <button className="w-fit rounded-none border-[1px] border-[#000] px-6 py-3 text-sm font-thin transition duration-300 ease-in-out hover:bg-black hover:text-white mt-4">View More</button>
-            </div>
-            <div className="col-span-12 flex justify-center">
-                <img
-                    style={{ width: 100, height: 100 }}
-                    src={"2.png"}
-                    alt="Image"
-                    className="rounded-2xl object-cover brightness-75"
-                />
+        <div className="absolute h-screen">
+            <AboutBg />
+
+            <div className="grid grid-cols-12 md:col-span-3 gap-3 relative">
+                <div className="col-span-12 flex md:col-span-6 flex-col items-center justify-center px-5 md:px-10 hover:scale-105 transition-transform duration-300">
+                    <h3 className="text-center relative z-10">
+                        <span className="text-1xl font-semibold md:my-3 md:text-4xl md:leading-[100px]" style={{ color: 'black' }}>Elevating Vehicles For </span>
+                        <span className="text-1xl font-semibold md:my-3 md:text-4xl md:leading-[100px]" style={{ color: '#ac9b79' }}>Max Performance</span>
+                        <span className="text-center mb-2 h-1 w-5 rounded-full bg-white" style={{ display: 'inline-block', width: '100%', textAlign: 'center' }}>
+          At Elevated Motoring,
+          we are dedicated to providing
+          high-quality service for your
+          vehicle needs. With 35+ years
+          of combined experience in the
+          automotive industry, our knowledgeable
+          staff can assist you with OEM and aftermarket
+          solutions tailored to your vehicle’s requirements.
+          Trust us to deliver exceptional results and elevate
+          your driving experience.
+        </span>
+                    </h3>
+                    <button className="w-fit rounded-none border-[1px] border-[#000] px-6 py-3 text-sm font-thin transition duration-300 ease-in-out hover:bg-black hover:text-white mt-4">View More</button>
+                </div>
+                <div className="col-span-12 flex justify-center hover:scale-105 transition-transform duration-300">
+                    <img
+                        style={{ width: 500, height: 500, transform: 'translate(0%, 0%)' }}
+                        src={"2.png"}
+                        alt="Image"
+                        className="rounded-2xl object-cover brightness-75"
+                    />
+                </div>
             </div>
         </div>
-
-
     );
 }
