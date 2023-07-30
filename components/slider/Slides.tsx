b6fb67a4fff0407bd0ff1cbae722b379348e8c74
mt_tarik <skorohodit12@gmail.com>
2023-07-30 11:26:49 +0300

    FixAnimation

--- components/slider/Slides.tsx
+++ components/slider/Slides.tsx
@@ -1,20 +1,19 @@
-
 import React from "react";
 import SliderCard from "./SliderCard";
 import {Data} from "@/components/slider/Sliders";
 
 type Props = {
-    data: Data[];
+  data: Data[];
 };
 
 function Slides({ data }: Props) {
-    return (
-        <div className=" flex w-full gap-6">
-            {data.map((data) => {
-                return <SliderCard key={data.img} data={data} />;
-            })}
-        </div>
-    );
+  return (
+    <div className=" flex w-full gap-6">
+      {data.map((data) => {
+        return <SliderCard key={data.img} data={data} />;
+      })}
+    </div>
+  );
 }
 
 export default Slides;
