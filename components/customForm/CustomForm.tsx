6ac64f921edd2a25190917cadaecaa60f7b254b7
mt_tarik <skorohodit12@gmail.com>
2023-09-29 18:49:37 +0300

    telegramPh

--- components/customForm/CustomForm.tsx
+++ components/customForm/CustomForm.tsx
@@ -108,56 +108,47 @@ export const CustomForm = () => {
     try {
       const message = `
       Contact Details:
-  
       -First Name: ${data.firstName}
       -Last Name: ${data.lastName}
       -Phone: ${data.phone}
       -Email: ${data.email}
 
       Vehicle Information:
-  
       -Year: ${data.year}
       -Make: ${data.make}
       -Model: ${data.model}
       -License Plate: ${data.licensePlate}
       -State: ${data.state}
-  
+
       Services:
-  
       ${data.services.map((service: string) => `- ${service}`).join('\n')}
 
       Comments:
       (${data.comment})
     `;
+
       const photoArray = Array.from(data.file) as File[];
 
       const mediaGroup = photoArray.map((photo: File, index: number) => ({
         type: 'photo',
         media: `attach://photo_${index}`,
+        caption: message,
       }));
-      const textFormData = new FormData();
-      textFormData.append('chat_id', chatId);
-      textFormData.append('text', message);
 
-      const textResponse = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-        method: 'POST',
-        body: textFormData,
-      });
-
-      if (!textResponse.ok) {
-        console.error('Telegram API returned an error for sending text:', textResponse.statusText);
-        return; // Зупинити виконання, якщо відправлення тексту не вдалося
-      }
       const formData = new FormData();
       formData.append('chat_id', chatId);
-      formData.append('media', JSON.stringify(mediaGroup));
 
+      // Додаємо текстове повідомлення
+      formData.append('text', message);
+
+      // Додаємо медіа для кожної фотографії
       photoArray.forEach((photo: File, index: number) => {
         formData.append(`photo_${index}`, photo);
       });
 
-
+      // Додаємо інформацію про медіагрупу
       formData.append('media', JSON.stringify(mediaGroup));
+
       const response = await fetch(`https://api.telegram.org/bot${botToken}/sendMediaGroup`, {
         method: 'POST',
         body: formData,
@@ -166,12 +157,13 @@ export const CustomForm = () => {
       if (!response.ok) {
         console.error('Telegram API returned an error for sending photos:', response.statusText);
       } else {
-        console.log('Telegram message with photos sent successfully');
+        console.log('Telegram message with text and photos sent successfully');
       }
     } catch (error) {
       console.error('Error sending Telegram message:', error);
     }
   });
+
 
 
 
