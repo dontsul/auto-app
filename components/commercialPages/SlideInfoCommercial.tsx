d259212deb37c717a268a33cbab69bd0525797a0
mt_tarik <skorohodit12@gmail.com>
2023-10-02 14:05:36 +0300

    FixPortfolioа

--- components/commercialPages/SlideInfoCommercial.tsx
+++ components/commercialPages/SlideInfoCommercial.tsx
@@ -15,7 +15,20 @@ function SlideInfoCommercial({ transitionData, currentSlideData }: Props) {
             <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
             <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
             <motion.div layout className="mt-5 flex items-center gap-3">
-
+                <LinkScroll
+                    activeClass="active"
+                    to="form"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
+                >
+                    <button
+                        className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
+                    >
+                        Contact us
+                    </button>
+                </LinkScroll>
             </motion.div>
         </>
     );
