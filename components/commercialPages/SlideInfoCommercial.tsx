deb036612acce4a53ead5fbe63ce764f3c291ad7
mt_tarik <skorohodit12@gmail.com>
2023-10-02 13:53:21 +0300

    FixPortfolioа

--- components/commercialPages/SlideInfoCommercial.tsx
+++ components/commercialPages/SlideInfoCommercial.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { CurrentSlideData, DataSlider } from "@/data/dataSliderHome/DataTepe";
 import OtherInfo from "@/components/slider/OtherInfo";
+import {CurrentSlideDataC, DataSliderC} from "@/data/dataSliderCommercial/DataTepe";
+import {Link as LinkScroll} from "react-scroll/modules";
 
 type Props = {
-    transitionData: DataSlider;
-    currentSlideData: CurrentSlideData;
+    transitionData: DataSliderC;
+    currentSlideData: CurrentSlideDataC;
 };
 
-function SlideInfo({ transitionData, currentSlideData }: Props) {
+function SlideInfoCommercial({ transitionData, currentSlideData }: Props) {
     return (
         <>
             <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
-            <OtherInfo
-                data={transitionData ? transitionData : currentSlideData.data}
-            />
+            <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
             <motion.div layout className="mt-5 flex items-center gap-3">
-                <button
-                    className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
-                    onClick={() =>
-                        document
-                            .getElementById("form") // Replace with the ID of your form section
-                            ?.scrollIntoView({ behavior: "smooth" })
-                    }
+                <LinkScroll
+                    activeClass="active"
+                    to="form"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
                 >
-                    Contact us
-                </button>
+                    <button
+                        className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
+                    >
+                        Contact us
+                    </button>
+                </LinkScroll>
             </motion.div>
         </>
     );
 }
 
-export default SlideInfo;
+export default SlideInfoCommercial;
--- components/slider/SlideInfo.tsx
+++ components/slider/SlideInfo.tsx
@@ -3,7 +3,7 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 import { Link as LinkScroll } from "react-scroll";
 import OtherInfo from "./OtherInfo";
-import { CurrentSlideData, DataSlider } from "@/data/dataSliderHome/DataTepe";
+import {CurrentSlideData, DataSlider} from "@/data/dataSliderHome/DataTepe";
 
 type Props = {
     transitionData: DataSlider;
@@ -14,9 +14,7 @@ function SlideInfo({ transitionData, currentSlideData }: Props) {
     return (
         <>
             <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
-            <OtherInfo
-                data={transitionData ? transitionData : currentSlideData.data}
-            />
+            <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
             <motion.div layout className="mt-5 flex items-center gap-3">
                 <Link href={transitionData.link} passHref>
                     <button
@@ -25,16 +23,20 @@ function SlideInfo({ transitionData, currentSlideData }: Props) {
                         {transitionData.title}
                     </button>
                 </Link>
-                <button
-                    className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
-                    onClick={() =>
-                        document
-                            .getElementById("form") // Replace with the ID of your form section
-                            ?.scrollIntoView({ behavior: "smooth" })
-                    }
+                <LinkScroll
+                    activeClass="active"
+                    to="form"
+                    spy={true}
+                    smooth={true}
+                    offset={-70}
+                    duration={500}
                 >
-                    Contact us
-                </button>
+                    <button
+                        className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
+                    >
+                        Contact us
+                    </button>
+                </LinkScroll>
             </motion.div>
         </>
     );
