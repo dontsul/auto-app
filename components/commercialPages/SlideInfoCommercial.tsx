e3b24587d15e834fd15116d043c3f84d39851823
mt_tarik <skorohodit12@gmail.com>
2023-10-02 13:48:05 +0300

    FixPortfolioа

--- components/commercialPages/SlideInfoCommercial.tsx
+++ components/commercialPages/SlideInfoCommercial.tsx
@@ -1,35 +1,34 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Link as LinkScroll } from "react-scroll";
+import { CurrentSlideData, DataSlider } from "@/data/dataSliderHome/DataTepe";
 import OtherInfo from "@/components/slider/OtherInfo";
-import {CurrentSlideDataC, DataSliderC} from "@/data/dataSliderCommercial/DataTepe";
 
 type Props = {
-    transitionData: DataSliderC;
-    currentSlideData: CurrentSlideDataC;
+    transitionData: DataSlider;
+    currentSlideData: CurrentSlideData;
 };
 
-function SlideInfoCommercial({ transitionData, currentSlideData }: Props) {
+function SlideInfo({ transitionData, currentSlideData }: Props) {
     return (
         <>
             <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
-            <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
+            <OtherInfo
+                data={transitionData ? transitionData : currentSlideData.data}
+            />
             <motion.div layout className="mt-5 flex items-center gap-3">
-                <LinkScroll
-                    activeClass="active"
-                    to="form"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={500}
+                <button
+                    className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black"
+                    onClick={() =>
+                        document
+                            .getElementById("form") // Replace with the ID of your form section
+                            ?.scrollIntoView({ behavior: "smooth" })
+                    }
                 >
-                    <button className="w-full md:w-auto rounded-full border-[1px] border-[#ffffff8f] px-8 py-4 text-[14px] font-thin transition duration-300 ease-in-out hover:bg-white hover:text-black">
-                        Contact us
-                    </button>
-                </LinkScroll>
+                    Contact us
+                </button>
             </motion.div>
         </>
     );
 }
 
-export default SlideInfoCommercial;
+export default SlideInfo;
