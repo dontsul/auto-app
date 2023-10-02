85d9417fafe52dc8746d21e611b7244e712e1718
mt_tarik <skorohodit12@gmail.com>
2023-10-02 14:09:09 +0300

    FixPortfolioа

--- components/commercialPages/SlideInfoCommercial.tsx
+++ components/commercialPages/SlideInfoCommercial.tsx
@@ -11,11 +11,7 @@ type Props = {
 function SlideInfoCommercial({ transitionData, currentSlideData }: Props) {
     return (
         <>
-            <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
             <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
-            {/*<motion.div layout className="mt-5 flex items-center gap-3">*/}
-
-            {/*</motion.div>*/}
         </>
     );
 }
