39ff0e1ae069f98eb772b21f0779523407dff3c7
mt_tarik <skorohodit12@gmail.com>
2023-10-02 13:38:17 +0300

    FixPortfolioа

--- components/slider/SlideInfo.tsx
+++ components/slider/SlideInfo.tsx
@@ -13,7 +13,7 @@ type Props = {
 function SlideInfo({ transitionData, currentSlideData }: Props) {
     return (
         <>
-            {/*<motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />*/}
+            <motion.span layout className="mb-2 h-1 w-5 rounded-full bg-white" />
             <OtherInfo data={transitionData ? transitionData : currentSlideData.data} />
             <motion.div layout className="mt-5 flex items-center gap-3">
                 <Link href={transitionData.link} passHref>
