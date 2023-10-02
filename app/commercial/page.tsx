ef761d6eec828a5f7ec931634fb2b15d7fbbca6b
mt_tarik <skorohodit12@gmail.com>
2023-10-02 13:15:58 +0300

    FixPortfolio

--- app/commercial/page.tsx
+++ app/commercial/page.tsx
@@ -1,14 +1,15 @@
 import {AboutSection} from "@/components/aboutSection/AboutSection";
 import {Contact} from "@/components/contact/Contact";
-import {Reviews} from "@/components/reviews/Reviews";
 import React from "react";
 import CommercialSlider from "@/components/commercialPages/CommercialSlider";
+import {PartnersSlider} from "@/components/partnersSlider/PartnersSlider";
 
 export default function Financing() {
   return (
       <div>
           <CommercialSlider />
           <AboutSection />
+          <PartnersSlider />
           <Contact />
       </div>
   );
