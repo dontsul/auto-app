ed3618f958d7927a19fbd7b9591ac989d5845ab1
mt_tarik <skorohodit12@gmail.com>
2023-10-20 14:03:09 +0300

    FixPor

--- components/partnersSlider/partnersSliderList/PartnersSliderList.tsx
+++ components/partnersSlider/partnersSliderList/PartnersSliderList.tsx
@@ -74,7 +74,7 @@ export const PartnersSliderList: FC<IPartnersListProps> = ({ partners }) => {
           className="py-4 z-30 sm:px-8 lg:px-0"
       >
         <Slider className="py-6" {...settings}>
-          {partners?.map((partner) => (
+          {partners && partners?.map((partner) => (
               <ItemPartnersSlider partner={partner} key={uuidv4()} />
           ))}
         </Slider>
