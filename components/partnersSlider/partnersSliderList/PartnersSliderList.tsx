aaee2b2e99a94542d5b9a29af691d1281e4e3e19
Vasyl Dontsul <dontsul.v@gmail.com>
2023-10-21 22:40:50 +0300

    fix

--- components/partnersSlider/partnersSliderList/PartnersSliderList.tsx
+++ components/partnersSlider/partnersSliderList/PartnersSliderList.tsx
@@ -74,9 +74,9 @@ export const PartnersSliderList: FC<IPartnersListProps> = ({ partners }) => {
       className="py-4 z-30 sm:px-8 lg:px-0"
     >
       <Slider className="py-6 sl-btn" {...settings}>
-        {partners.map((partner) => (
-          <ItemPartnersSlider partner={partner} key={uuidv4()} />
-        ))}
+        {partners.map((partner) => {
+          return <ItemPartnersSlider partner={partner} key={uuidv4()} />;
+        })}
       </Slider>
     </motion.div>
   );
